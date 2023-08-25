417379bc0ba6cedcb7216282f6009494c2d6fba4
Saif <skazmi21@illinois.edu>
2023-08-25 09:35:32 -0500

    able to check when user joins, leaves and how long they were in voice

--- health_bot.py
+++ health_bot.py
@@ -1,19 +1,29 @@
 import discord
 import os
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 client = discord.Client()
+client.start_time = 0
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(client))
 
 @client.event
 async def on_voice_state_update(member, before, after):
+        #Sends message if user with given id JOINS voice
         if not before.channel and after.channel and member.id == 283363939163701249:
             channel = client.get_channel(1142138293581004901)
-            await channel.send('KUMANZI IS HERE')
+            await channel.send('kumanzi is here')
+            client.start_time = time.time()
+        #Sends message if user with given id LEAVES voice
+        if before.channel and not after.channel and member.id == 283363939163701249:
+            channel = client.get_channel(1142138293581004901)
+            end_time = time.time()
+            elapsed_time = end_time - client.start_time
+            await channel.send(f'kumanzi is gone (in voice for {elapsed_time:.2f} seconds)')
 
 intents = discord.Intents.default()
 intents.members = True
